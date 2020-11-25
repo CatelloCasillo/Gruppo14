cc5214244e75b67e997b2904fafc6bd0a12627ef
Enrico <Enrico@DESKTOP-SI688BV>
2020-11-25 19:10:36 +0100

    modification of repository

---
+++ ProgettoSEGruppo15/src/ClassMainEnrico.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Enrico
+ */
+public class ClassMainEnrico {
+    public static void main(String[] args) {
+        Repository r= new Repository();
+        r.getMaintenanceActivity();
+    }
+}
