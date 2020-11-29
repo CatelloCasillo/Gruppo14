a1b344c8287c739f20ed530ac2c61406acc8a0e0
GabriellaDeMarco <gabriellademarco46@gmail.com>
2020-11-29 01:08:43 +0100

    ora dovrebbe essere più ordinato!

---
+++ ProgettoSEGruppo15/test/FirstPagePlannerGUITest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import plannerGUI.FirstPagePlannerGUI;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author User
+ */
+public class FirstPagePlannerGUITest{
+    
+    public FirstPagePlannerGUITest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of borderWhite method, of class FirstPagePlannerGUI.
+     */
+    @Test
+    public void testBorderWhite() {
+        System.out.println("borderWhite");
+        FirstPagePlannerGUI instance = null;
+        instance.borderWhite();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of borderBlack method, of class FirstPagePlannerGUI.
+     */
+    @Test
+    public void testBorderBlack() {
+        System.out.println("borderBlack");
+        FirstPagePlannerGUI instance = null;
+        instance.borderBlack();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class FirstPagePlannerGUI.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        FirstPagePlannerGUI.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
