55ed634eb16a614ad461cd574b3d89fcfe414eb7
Enrico <Enrico@DESKTOP-SI688BV>
2020-12-01 20:27:11 +0100

    repository changes

--- ProgettoSEGruppo15/src/Repository/Repository.java
+++ ProgettoSEGruppo15/src/Repository/Repository.java
@@ -16,6 +16,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -44,7 +45,11 @@ public class Repository {
             Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-   
+
+    public Statement getStm() {
+        return stm;
+    }
+    
     public ResultSet getInformationOfMaintenanceActivity(){
        
         try {
@@ -176,7 +181,7 @@ public class Repository {
         }
         
     }
-    public void insertNewMaintenanceActivity(String activityID,
+    public boolean insertNewMaintenanceActivity(String activityID,
                // String plannerID, String maintainerID, String procedureID,
                 String siteID,
                 String activityDescription,int activityInterventionTime,
@@ -208,32 +213,37 @@ public class Repository {
         temp.append("'").append(activityWorkspaceNotes).append("',");
         temp.append("'").append(activityTypology).append("' ");
         temp.append(");");
-        viewMaintenanceActivityTable();
+        
+        //viewMaintenanceActivityTable();
         try {
             stm.executeUpdate(temp.toString());
+            return true;
         } catch (SQLException ex) {
             Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
         }
     }
     
-    public void deleteMaintenanceActivity(String activityID){
+    public boolean deleteMaintenanceActivity(String activityID){
         StringBuilder temp = new StringBuilder();
-        System.out.println(activityID);
+        //System.out.println(activityID);
         temp.append("delete from MaintenanceActivity"
                 + " where activityid = ");
         temp.append(" '").append(activityID).append("' ;");
-        System.out.println(temp);
+        //System.out.println(temp);
         
                 
         try {
             stm.executeUpdate(temp.toString());
+            return true;
         } catch (SQLException ex) {
             Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
         }
     }
     
     
-    public void updateMaintenanceActivity(String id,String typology, String description, int time, boolean inter, int week){
+    public boolean updateMaintenanceActivity(String id,String typology, String description, int time, boolean inter, int week){
         StringBuilder temp = new StringBuilder();
         temp.append("update MaintenanceActivity"
         + " set activityDescription = ");
@@ -246,13 +256,15 @@ public class Repository {
         
          try {
             stm.executeUpdate(temp.toString());
+            return true;
         } catch (SQLException ex) {
             Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
         }
         
     }
     
-    public void viewMaintenanceActivityTable(){
+    public boolean viewMaintenanceActivityTable(){
             
         try {
             String query = "select* from "
@@ -273,8 +285,10 @@ public class Repository {
             System.out.println(activityID+"\t"+siteID+"\t"+activityDescription+"\t"+activityInterventionTime+"\t"+interruptibleActivity+
             "\t"+activityWeekNumber+"\t"+activityTypology);
             } 
+            return true;
         } catch (SQLException ex) {
             Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
         }
     }
     public ResultSet getSiteTable(){
@@ -284,6 +298,25 @@ public class Repository {
             ResultSet rst = stm.executeQuery(query);
             return rst;
         
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public ArrayList<String> getCompetencesOfActivity(String activityID){
+        ArrayList<String> skillList = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        temp.append("select c.competenceName from \n" +
+                    "MaintenanceActivity as ma \n" +
+                    "join competenceToProcedure as cp on (ma.procedureID=cp.procedureID )\n" +
+                    "join competence as c on (c.competenceID=cp.competenceID ) ");
+        temp.append(" where ma.activityID= '").append(activityID).append("' ;");
+         try {
+            ResultSet rst = stm.executeQuery(temp.toString());
+            while(rst.next()){
+                skillList.add(rst.getString(0));
+            }
+            return skillList;
         } catch (SQLException ex) {
             Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
             return null;
--- ProgettoSEGruppo15/test/RepositoryTest.java
+++ ProgettoSEGruppo15/test/RepositoryTest.java
@@ -4,22 +4,31 @@
  * and open the template in the editor.
  */
 
+import Repository.Repository;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.postgresql.core.v3.QueryExecutorImpl;
+import org.postgresql.util.PSQLException;
 
 /**
  *
  * @author Enrico
  */
 public class RepositoryTest {
-    
-    public RepositoryTest() {
-    }
-    
+    private Repository repo;
+    private ResultSet rstFull;
+    private ResultSet rstEmpty;
+   
+
+   
     @BeforeClass
     public static void setUpClass() {
     }
@@ -30,16 +39,200 @@ public class RepositoryTest {
     
     @Before
     public void setUp() {
+        repo= new Repository();
+        //rst = repo.getInformationOfMaintenanceActivity();
+        try {
+            initTestTables();
+            //creazione di un result set riempito per il testing
+            initTestResultSet();
+            rstFull.next();
+            //creazione di un result set vuoto per il testing
+            initTestResultSetEmpty();
+            rstEmpty.next();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(RepositoryTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     @After
     public void tearDown() {
+       deleteTestTables();
     }
+    public void initTestTables(){
+        String query;
+        query="insert into Planner\n" +
+                "values ('test01','1234');\n" +
+                "\n" +
+                "insert into Site\n" +
+                "values ('test02','factory1','area1');\n" +
+                "\n" +
+                "insert into Procedure\n" +
+                "values ('test03','proceduraTest','testoProcedura','ulrFileSMP');\n" +
+                "\n" +
+                "insert into Competence\n" +
+                "values ('test04','nomeCompetenza','descrizioneCompetenza');\n" +
+                "\n" +
+                "insert into competenceToProcedure\n" +
+                "values('test03','test04');\n" +
+                "\n" +
+                "insert into Maintainer\n" +
+                "values ('test05','pippo','1234');"
+                + "insert into MaintenanceActivity(activityID,activityDescription,activityInterventionTime,\n" +
+                "interruptibleActivity,activityWeekNumber,activityTypology,siteID,procedureID,plannerID,MaintainerID,activityMaterials,activityWorkspaceNotes)\n" +
+                "values ('test06','descrizioneAttivita',120,true,22,'electrical','test02','test03','test01','test05','meterialsTest','notes');";
+        try {
+            repo.getStm().executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void deleteTestTables(){
+        String query;
+        query="delete from competencetoprocedure where procedureid ='test03';"
+                + " delete from MaintenanceActivity where activityid = 'test06';"
+                + " delete from Planner where plannerid = 'test01';"
+                + " delete from Site where siteid = 'test02';"
+                + " delete from Procedure where Procedureid = 'test03';"
+                + " delete from Competence where Competenceid = 'test04';"
+                + " delete from Maintainer where Maintainerid = 'test05';"
+               ;
+                
+        try {
+            repo.getStm().executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void initTestResultSet(){
+          try {
+            String query = "select* from \n" +
+                            "MaintenanceActivity as ma \n" +
+                            " full join Procedure as p on (ma.procedureID=p.procedureID )\n" +
+                            " full join Site as s on (ma.siteID=s.siteID )\n"+
+                            " where ma.activityid='test06' " +
+                            "order by ma.activityID; ";
+                   
+            rstFull = repo.getStm().executeQuery(query);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void initTestResultSetEmpty(){
+      try {
+         //query utilizzata per ottenere 0 righe
+        String query = "select* from MaintenanceActivity where activityWeekNumber<0;";
+               /* "select* from \n" +
+                            "MaintenanceActivity as ma \n" +
+                            " full join Procedure as p on (ma.procedureID=p.procedureID )\n" +
+                            " full join Site as s on (ma.siteID=s.siteID )\n"+
+                            " where ma.activityWeekNumber<0 " +
+                            "order by ma.activityID; ";
+                  */      
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        rstEmpty = repo.getStm().executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    @Test
+    public void testSetRepositoryConnection() {
+        String url = "jdbc:postgresql://localhost/Progetto_SE_gruppo14";
+        String user = "utente_progetto_se";
+        String pwd = "password";
+        repo.setRepositoryConnection(url,user,pwd);
+    }
+    /*
+    @Test(expected = SQLException.class)
+    public void testWrongURLRepositoryConnection() {
+        String url = "jdbc:postgresql://localhost/fail";
+        String user = "utente_progetto_se";
+        String pwd = "password";
+        repo.setRepositoryConnection(url,user,pwd);
+    }
+    */
     
+    @Test
+    public void testGetInformationOfMaintenanceActivity() {
+        assertNotNull(repo.getInformationOfMaintenanceActivity());
+    }
+    @Test
+    public void testGetActivityID() {
+       assertNotNull(repo.getActivityID(rstFull));
+       assertNull(repo.getActivityID(rstEmpty));
+    }
+    @Test
+    public void testGetActivityDescription() {
+        assertNotNull(repo.getActivityDescription(rstFull));
+       // assertNull(repo.getActivityDescription(rstEmpty));
+    }
+    @Test
+    public void testGetActivityInterventionTime() {
+        assertNotNull(repo.getActivityInterventionTime(rstFull));
+        //assertEquals(0,repo.getActivityInterventionTime(rstEmpty));
+    }
+    @Test
+    public void testIsInterruptibleActivity() {
+        assertNotNull(repo.isInterruptibleActivity(rstFull));
+        //assertEquals(false,repo.isInterruptibleActivity(rstEmpty));
+    }
+    @Test
+    public void testGetActivityWeekNumber() {
+        assertNotNull(repo.getActivityWeekNumber(rstFull));
+        //assertEquals(0,repo.getActivityWeekNumber(rstEmpty));
+    }
+    @Test
+    public void testGetActivityTypology() {
+        assertNotNull(repo.getActivityTypology(rstFull));
+        //assertNull(repo.getActivityTypology(rstEmpty));
+    }
+    @Test
+    public void testGetSiteID() {
+        assertNotNull(repo.getSiteID(rstFull));
+        //assertNull(repo.getSiteID(rstEmpty));
+    }
+    @Test
+    public void testGetFactorySite() {
+        assertNotNull(repo.getFactorySite(rstFull));
+        //assertNull(repo.getFactorySite(rstEmpty));
+    }
+    @Test
+    public void testGetAreaSite() {
+        assertNotNull(repo.getAreaSite(rstFull));
+        //assertNull(repo.getAreaSite(rstEmpty));
+    }
+    @Test
+    public void testGetProcedureID() {
+        assertNotNull(repo.getProcedureID(rstFull));
+        //assertNull(repo.getProcedureID(rstEmpty));
+    }
+    @Test
+    public void testGetProcedureName() {
+        assertNotNull(repo.getProcedureName(rstFull));
+        //assertNull(repo.getProcedureName(rstEmpty));
+    }
+    @Test
+    public void testGetProcedureDescription() {
+        assertNotNull(repo.getProcedureDescription(rstFull));
+        //assertNull(repo.getProcedureDescription(rstEmpty));
+    }
+    @Test
+    public void testGetFileSMP() {
+        assertNotNull(repo.getFileSMP(rstFull));
+        //assertNull(repo.getFileSMP(rstEmpty));
+    }
+    @Test
+    public void testGetSiteTable() {
+        assertNotNull(repo.getSiteTable());
+    }
+    @Test
+    public void testInsertUpdateDeleteActivityTable() {
+        //siteID='test02' nella tabella di test
+        //weekNumber deve essere compreso tra 1 e 52 inclusi
+        repo.insertNewMaintenanceActivity("test99", "test02", "descriptionTest", 60, true, 10, "notes", "typology");
+        repo.updateMaintenanceActivity("test99", "changed typology", "changed description", 70, false, 11);
+        repo.deleteMaintenanceActivity("test99");
+        
+    }
 }
