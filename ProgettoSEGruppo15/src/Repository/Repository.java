f3924f8a7afa2e34c7346d7a3bc10b53c9e621c5
Enrico <Enrico@DESKTOP-SI688BV>
2020-12-11 19:59:10 +0100

    repository functions for maintainer

--- ProgettoSEGruppo15/src/Repository/Repository.java
+++ ProgettoSEGruppo15/src/Repository/Repository.java
@@ -321,4 +321,346 @@ public class Repository {
             return null;
         }
     }
+
+    //Sprint 2
+  public ResultSet getMaintainerTable(){
+         try {
+            String query = "select maintainerId, maintainerName from Maintainer; ";
+                   
+            ResultSet rst = stm.executeQuery(query);
+            return rst;
+        
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public ResultSet getAllMaintainersCurrentWeekDayAvailability(String day){
+        //check day string
+        String days[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        for(int i=0;i>7;i++){
+            if(!days[i].equals(day)){
+                System.err.println("ERROR: day does not exist, try one of these -> \"Monday\",\"Tuesday\",\"Wednesday\",\"Thursday\",\"Friday\",\"Saturday\",\"Sunday\"");
+                return null;
+            }
+        }
+      
+        StringBuilder temp = new StringBuilder();
+        temp.append("select m.maintainerID,ma.TimeSlot1,ma.TimeSlot2,ma.TimeSlot3,ma.TimeSlot4,ma.TimeSlot5,ma.TimeSlot6,ma.TimeSlot7,ma.TimeSlot8\n" +
+                    " from MaintainerAvailability as ma\n" +
+                    " full join Maintainer as m on (m.maintainerID=ma.maintainerID )\n" +
+                    " where (ma.day='").append(day).append("' ");
+	temp.append(" order by m.maintainerID; ");
+        
+        try {
+            return stm.executeQuery(temp.toString());
+            
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public ResultSet getDayAvailability(String mainteinerID, String day){
+        //check day string
+        String days[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        for(int i=0;i>7;i++){
+            if(!days[i].equals(day)){
+                System.err.println("ERROR: day does not exist, try one of these -> \"Monday\",\"Tuesday\",\"Wednesday\",\"Thursday\",\"Friday\",\"Saturday\",\"Sunday\"");
+                return null;
+            }
+        }
+      
+        StringBuilder temp = new StringBuilder();
+        temp.append("sselect ma.maintainerID,ma.day,ma.TimeSlot1,ma.TimeSlot2,ma.TimeSlot3,ma.TimeSlot4,ma.TimeSlot5,ma.TimeSlot6,ma.TimeSlot7,ma.TimeSlot8\n" +
+                    " from MaintainerAvailabilityCurrentWeek as ma\n" +
+                    " where ma.day= '").append(day).append("' and ma.maintainerID= '").append(mainteinerID).append("';");
+        try {
+           return stm.executeQuery(temp.toString());
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public ResultSet getWeekAvailability(String mainteinerID){
+        StringBuilder temp = new StringBuilder();
+        temp.append("sselect ma.maintainerID,ma.day,ma.TimeSlot1,ma.TimeSlot2,ma.TimeSlot3,ma.TimeSlot4,ma.TimeSlot5,ma.TimeSlot6,ma.TimeSlot7,ma.TimeSlot8\n" +
+                    " from MaintainerAvailabilityCurrentWeek as ma\n" +
+                    " where ma.maintainerID= '").append(mainteinerID).append("';");
+        try {
+            return stm.executeQuery(temp.toString());
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public boolean updateMaintainerAvailabilityCurrentWeek(String maintainerID, String day,
+                    int timeSlot1,int timeSlot2,int timeSlot3,int timeSlot4,int timeSlot5,int timeSlot6,int timeSlot7,int timeSlot8 ){
+        StringBuilder temp = new StringBuilder();
+        temp.append("update MaintainerAvailabilityCurrentWeek"
+        + " set");
+        temp.append(" timeSlot1 =").append(timeSlot1).append(",");
+        temp.append(" timeSlot2 =").append(timeSlot2).append(",");
+        temp.append(" timeSlot3 =").append(timeSlot3).append(",");
+        temp.append(" timeSlot4 =").append(timeSlot4).append(",");
+        temp.append(" timeSlot5 =").append(timeSlot5).append(",");
+        temp.append(" timeSlot6 =").append(timeSlot6).append(",");
+        temp.append(" timeSlot7 =").append(timeSlot7).append(",");
+        temp.append(" timeSlot8 =").append(timeSlot8).append(" ");
+        temp.append(" where maintainerID = '").append(maintainerID).append("' and day= '").append(day).append("' ;");
+        
+         try {
+            stm.executeUpdate(temp.toString());
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+    }
+    public boolean resetMaintainerAvailabilityCurrentWeek(String maintainerID, String day){
+        StringBuilder temp = new StringBuilder();
+        temp.append("update MaintainerAvailabilityCurrentWeek"
+        + " set");
+        temp.append(" timeSlot1 = 60,");
+        temp.append(" timeSlot2 = 60,");
+        temp.append(" timeSlot3 = 60,");
+        temp.append(" timeSlot4 = 60,");
+        temp.append(" timeSlot5 = 60,");
+        temp.append(" timeSlot6 = 60,");
+        temp.append(" timeSlot7 = 60,");
+        temp.append(" timeSlot8 = 60 ");
+        temp.append(" where maintainerID = '").append(maintainerID).append("' and day= '").append(day).append("' ;");
+        
+         try {
+            stm.executeUpdate(temp.toString());
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+    }
+    public ResultSet getCompetencesOfMaintainer(String maintainerID){
+      
+        StringBuilder temp = new StringBuilder();
+        temp.append("select c. competenceID , c.competenceName from \n" +
+                    "competenceToMaintainer as cm \n" +
+                    "join competence as c on (c.competenceID=cm.competenceID )\n" +
+                    "where cm.MaintainerID = '").append(maintainerID).append("' ;");
+        try {
+            return stm.executeQuery(temp.toString());
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getCompetenceID(ResultSet rst){
+        try {
+            return rst.getString("CompetenceID");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getCompetenceName(ResultSet rst){
+        try {
+            return rst.getString("CompetenceName");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public String getMaintainerID(ResultSet rst){
+        try {
+            return rst.getString("MaintainerID");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getMaintainerName(ResultSet rst){
+        try {
+            return rst.getString("MaintainerName");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot1(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot1");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot2(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot2");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot3(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot3");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot4(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot4");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot5(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot5");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot6(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot6");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot7(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot7");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getTimeSlot8(ResultSet rst){
+        try {
+            return rst.getString("TimeSlot8");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public String getDay(ResultSet rst){
+        try {
+            return rst.getString("day");
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    /*
+    public boolean isInitializedInMaintainerAvailability(String maintainerID, String day, int weekNumber){
+        StringBuilder temp = new StringBuilder();
+        temp.append("select * from MaintainerAvailability\n" +
+                    "where maintainerID='").append(maintainerID).append("' and day='").append(day).append("' and weekNumber= ").append(weekNumber).append(" ;");
+                
+        try {
+            ResultSet rst = stm.executeQuery(temp.toString());
+           //return false se il result set è vuoto
+            return rst.next();
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    */
+    
+    ///-----------Funzioni inserimento nuovo maintainer con creazione della disponibilità ----------------------------
+    
+    public boolean insertNewMaintainer(String id, String name, String password){
+        if(id.length()>6){
+            System.err.println("Mainteiner id ERROR: length exceeded");
+            return false;
+        }
+        String days[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        StringBuilder temp = new StringBuilder();
+        temp.append("insert into Maintainer\n" +
+                        "values ('").append(id).append("' , '").append(name).append("' , '").append(password).append("' );");
+        try {
+            //SQL transaction
+            conn.setAutoCommit(false);
+            stm.executeUpdate(temp.toString());
+            for(int i=0;i<7;i++){
+                insertNewMaintainerAvailability(id,days[i]);
+            }
+            conn.commit();
+            conn.setAutoCommit(true);
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    private boolean insertNewMaintainerAvailability(String maintainerID, String day) {
+      
+        StringBuilder temp = new StringBuilder();
+        temp.append("insert into MaintainerAvailabilityCurrentWeek(maintainerID,Day, \n" +
+                    " TimeSlot1,TimeSlot2,TimeSlot3,TimeSlot4,TimeSlot5,TimeSlot6,TimeSlot7,TimeSlot8)\n" +
+                    " values('").append(maintainerID).append("','").append(day).append("',").append("60,60,60,60,60,60,60,60);");
+        try {
+            stm.executeUpdate(temp.toString());
+            return true;
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    //------------------------------------------------------------------------------------------------
+    
+    public boolean assignActivity(String activityID, String maintainerID, String day, int weekNumber, 
+                        int SlotAssigned1, int SlotAssigned2, int SlotAssigned3, int SlotAssigned4, int SlotAssigned5, int SlotAssigned6, int SlotAssigned7, int SlotAssigned8){
+        //update of maintainerId in activity table
+        StringBuilder temp1 = new StringBuilder();
+        temp1.append("update MaintenanceActivity"
+        + " set maintainerID = ");
+        temp1.append(" '").append(maintainerID).append("'");
+        temp1.append(" where activityid = ").append(" '").append(activityID).append(" ';");
+        
+        // store the time division of the activity
+        StringBuilder temp2 = new StringBuilder();
+        temp2.append("insert into ActivityTimeDivision\n" +
+                     " values( ");
+        temp2.append(" '").append(activityID).append("', ");
+        temp2.append(" '").append(maintainerID).append("', ");
+        temp2.append(" '").append(day).append("', ");
+        temp2.append(weekNumber).append(", ");
+        temp2.append(SlotAssigned1).append(", ");
+        temp2.append(SlotAssigned2).append(", ");
+        temp2.append(SlotAssigned3).append(", ");
+        temp2.append(SlotAssigned4).append(", ");
+        temp2.append(SlotAssigned5).append(", ");
+        temp2.append(SlotAssigned6).append(", ");
+        temp2.append(SlotAssigned7).append(", ");
+        temp2.append(SlotAssigned8).append(" ");
+        temp2.append(" );");
+         try {
+            //SQL transaction
+            conn.setAutoCommit(false);
+            stm.executeUpdate(temp1.toString());
+            stm.executeUpdate(temp2.toString());
+            conn.commit();
+            conn.setAutoCommit(true);
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Repository.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    
+    }
 }
